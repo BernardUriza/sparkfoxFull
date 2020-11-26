6d9757d72c64def9f5d97f329959544eac31d553
BernardUriza <bernarduriza@gmail.com>
2020-11-26 11:51:02 -0600

    upgrade to aply banner config

--- vistas/modulos/inicio/imagen-vertical.php
+++ vistas/modulos/inicio/imagen-vertical.php
@@ -11,21 +11,30 @@
 	<div class="carousel-inner" role="listbox">
 		<?php
 	for ($IndexSlider=0; $IndexSlider < count($slider); $IndexSlider++) {
-	$slide=$slider[$IndexSlider]; ?>
-		<div class="carousel-item <?php echo $IndexSlider==0?"active":""; ?>" style=" background-image:  url('<?php echo $slide["rutaImagenPortada"] ?>')">
+		$slide=$slider[$IndexSlider];
+		$mostrarTitulo = intval($slide["tituloBool"])==1;
+		$mostrarDescripcion = intval($slide["descripcionBool"])==1;
+	?>
+		<div class="carousel-item <?php echo $IndexSlider==0?"active":""; ?> uri" uri="<?php echo $slide["uri"] ?>" style="cursor: pointer; background-image:  url('<?php echo $slide["rutaImagenPortada"] ?>')">
 			<div class="carousel-caption">
 				<div class="container">
 					<div class="row">
-						<div class="col-1  d-none d-md-block">
-							<img src="vistas/img/green.png" alt="" class="imagenverde-vert ">
-						</div>
+						<?php if ($mostrarTitulo) { ?>
+							<div class="col-1  d-none d-md-block">
+								<img src="vistas/img/green.png" alt="" class="imagenverde-vert ">
+							</div>
+						<?php } ?>
 						<div class="col-md-6 col-12">
+						<?php if ($mostrarTitulo) { ?>
 							<div class="row">
 								<div class="title-header-copy-2 col uri" uri="<?php echo $slide["uri"] ?>"><?php echo $slide["titulo"] ?></div>
 							</div>
+						<?php } ?>
+						<?php if ($mostrarDescripcion) { ?>
 							<div class="row">
 								<div class="col title-header-copy-3 pt-3 py-md-2"><?php echo $slide["descripcion"] ?></div>
 							</div>
+						<?php } ?>
 						</div>
 					</div>
 				</div>
